970121563115c0ca48415232347bc6ae56b2e51c
rosafy <emmadomeisen@gmail.com>
2025-03-20 15:35:56 +0100

    Started work on copying

--- comparetest.py
+++ comparetest.py
@@ -9,6 +9,14 @@ while True:
     tempclip = pyclip.paste()
     datafile = open("cbdata.json","r+")
     data = json.loads(datafile.read())
-    datafile.truncate(0)
-    datafile.seek(0)
-    json.dump(data,datafile,indent=4)
+    if not data["content"] == tempclip:
+        if tempclip == clipboard:
+            clipboard = data["content"]
+            pyclip.copy(clipboard)
+        else:
+            data["content"] = tempclip
+            clipboard = tempclip
+            datafile.truncate(0)
+            datafile.seek(0)
+            json.dump(data,datafile,indent=4)
+    time.sleep(1)

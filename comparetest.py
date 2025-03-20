a0cd8f9ee5f3b6fd5ff3a7b111c5c7e88fc6cc83
rosafy <emmadomeisen@gmail.com>
2025-03-20 14:11:46 +0100

    Implemented basic Json

--- comparetest.py
+++ comparetest.py
@@ -1,13 +1,9 @@
 import keyboard
+import json
 import pyperclip
 import pyautogui as pya
 import time
 while True:
-    if keyboard.is_pressed("ctrl+alt+c"):
-        while True:
-            if not keyboard.is_pressed("ctrl+alt+c"):
-                break
-        pya.hotkey('ctrl', 'c')
-        time.sleep(2)
-        print(pyperclip.paste())
-        break
+    datafile = open("cbdata.json","r+")
+    data = json.loads(datafile.read())
+    

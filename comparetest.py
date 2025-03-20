e1efa2d0c8f92bacd5480841df4271a82ac1b10c
rosafy <emmadomeisen@gmail.com>
2025-03-20 18:13:32 +0100

    Did some work

--- comparetest.py
+++ comparetest.py
@@ -4,17 +4,51 @@ import pyclip
 import pyautogui as pya
 import time
 import base64
+import os
+from pathlib import Path
 clipboard = ""
 while True:
-    tempclip = pyclip.paste()
     datafile = open("cbdata.json","r+")
+    tempclip = pyclip.paste()
     data = json.loads(datafile.read())
-    if not data["content"] == tempclip:
+    if data["type"] == "txt":
+        tc = tempclip.decode("utf-8")
+    else:
+        tc = base64.b64encode(tempclip).decode("utf-8")
+    if not data["content"] == tc and not data["contentb64"] == tc:
         if tempclip == clipboard:
-            clipboard = data["content"]
-            pyclip.copy(clipboard)
+            print("a")
+            if data["type"] == "txt":
+                clipboard = data["content"]
+                pyclip.copy(clipboard)
+            else:
+                clipboard = data["contentb64"]
+                pyclip.copy(base64.b64decode(clipboard.encode()))
         else:
-            data["content"] = tempclip
+            print("b")
+            try:
+                clipdecoded = tempclip.decode("utf-8")
+            except:
+                data["contentb64"] = base64.b64encode(tempclip).decode("utf-8")
+            else:
+                if "file://" in clipdecoded:
+                    testclipdecoded = clipdecoded.replace("file://","")
+                    testclipdecoded = testclipdecoded.replace("\u0000","")
+                    if Path(testclipdecoded).exists():
+                        with open(testclipdecoded,"rb") as tcdc:
+                            encoded = base64.b64encode(tcdc.read())
+                            data["content"] = clipdecoded
+                            data["contentb64"] = encoded.decode("utf-8")
+                            data["type"] = "bin"
+                            tcdc.close()
+                    else:
+                        data["content"] = clipdecoded
+                        data["type"] = "txt"
+                        data["contentb64"] = ""
+                else:
+                        data["content"] = clipdecoded
+                        data["type"] = "txt"
+                        data["contentb64"] = ""
             clipboard = tempclip
             datafile.truncate(0)
             datafile.seek(0)
--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-import pyclip
-try:
-    pyclip.paste().decode("utf-8")
-except:
-    cliptype = "bin"
-else:
-    cliptype = "txt"
-print(cliptype)
+string = "abc"
+string.replace("d","")
+print(string)

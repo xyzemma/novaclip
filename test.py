8367b8ee540f1027b2e3d79ee4c6b6e6e106813d
rosafy <emmadomeisen@gmail.com>
2025-03-21 15:02:05 +0100

    Tested imgbb implementation

--- test.py
+++ test.py
@@ -1,5 +1,33 @@
-string = "abc"
+'''string = "abc"
 string.replace("d","")
 print(string)
 test = urllib.request.urlopen("https://i.postimg.cc/fRB9ZHZ4/test-image-3061864-1280-1648860360.png")
-print(test.read())
+print(test.read())'''
+import requests
+import json
+import pyclip
+import urllib.request as ul
+from bs4 import BeautifulSoup
+x = pyclip.paste().decode("utf-8")
+pathtype = None
+if "\x00" in x:
+    x = x.replace("\x00","")
+try:
+    soup = BeautifulSoup(x, 'html.parser')
+    x_attrs = soup.find('img').attrs
+    x = x_attrs["src"]
+except:
+    x = x
+print(x)
+y = ul.urlopen(x)
+file = open("/home/timo/Downloads/osint-exercise-001-big-picture.jpeg","rb")
+params = {
+    'key': '5fd22077dd3563d8d44d78b4985e6fd8',
+}
+
+files = {
+    'image': (y),
+}
+
+response = requests.post('https://api.imgbb.com/1/upload', params=params, files=files)
+print(response.json())
